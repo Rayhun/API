2731c083303ab906f4cc7ffd2a695ffbd5104508
rayhu <rayhunkhan27@gmail.com>
2021-05-26 22:49:10 +0600

    #2 add model serializers

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -2,23 +2,8 @@ from rest_framework import serializers
 from .models import Article
 
 
-class ArticleSerializers(serializers.Serializer):
+class ArticleSerializers(serializers.ModelSerializer):
     """ Article Serializer """
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    email = serializers.EmailField(max_length=100)
-    date = serializers.DateField()
-
-    def create(self, validated_data):
-        """ Article Create """
-
-        return Article.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        """ Article Update  """
-
-        instance.title = validated_data.get("title", instance.title)
-        instance.author = validated_data.get("author", instance.author)
-        instance.email = validated_data.get("email", instance.email)
-        instance.save()
-        return instance
+    class Meta:
+        model = Article
+        fields = '__all__'
